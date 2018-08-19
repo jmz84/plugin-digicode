873be3ebb4958345493025ce8fa18b57216a2d0e
Unknown <jerome.martinez@live.fr>
2018-08-19 17:09:48 +0200

    tranform template into digicode

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -18,17 +18,17 @@
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
-function template_install() {
-    
+function digicode_install() {
+
 }
 
-function template_update() {
-    
+function digicode_update() {
+
 }
 
 
-function template_remove() {
-    
+function digicode_remove() {
+
 }
 
 ?>
